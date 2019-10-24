63218bb3e43def507ac3bf0c223165e98e6f1da6
sabraRathbun <sabra.rathbun@ruralsourcing.com>
2019-10-24 11:05:20 -0400

    Made JudgementPage run off of Judgement data access class

--- DogShow/JudgementPage.cs
+++ DogShow/JudgementPage.cs
@@ -1,4 +1,5 @@
-﻿using PetaPoco;
+﻿using Common_DogShow;
+using PetaPoco;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,53 +9,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Service_DogShow;
 
 namespace DogShow
 {
     public partial class JudgementPage : Form
     {
-        //List<DogBreed> nameList = new List<DogBreed>();
-        //PullSqlData pull = new PullSqlData();
-        //int dogID;
+        List<DogBreed> nameList = new List<DogBreed>();
+        Judgement judge = new Judgement();
+    
+        int dogID;
         public JudgementPage()
         {
             InitializeComponent();
         }
-        //protected override void OnHandleCreated(EventArgs e)
-        //{
-        //    base.OnHandleCreated(e);
-        //    nameList = pull.GetDogBreed();
-        //    for (int i = 0; i < nameList.Count; i++)
-        //    {
-        //        DogBreed dog = nameList.ElementAt(i);
-        //        competitorsBox.Items.Add(dog.DogName);
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            nameList = judge.GetDogBreedList();
 
-        //    }
+            for (int i = 0; i < nameList.Count; i++)
+            {
+                DogBreed dog = nameList.ElementAt(i);
+                competitorsBox.Items.Add(dog.DogName);
 
-        //}
+            }
+
+        }
 
         private void selectDogBtn_Click(object sender, EventArgs e)
         {
-            //string dogName = competitorsBox.Text;
-            //dogID = pull.DogIdNum(dogName);
-            //sizePointTxt.Enabled = true;
-            //eyePointTxt.Enabled = true;
-            //furColorPointTxt.Enabled = true;
-            //furLengthPointTxt.Enabled = true;
+            string dogName = competitorsBox.Text;
+            dogID = judge.GetDogID(dogName);
+            sizePointTxt.Enabled = true;
+            eyePointTxt.Enabled = true;
+            furColorPointTxt.Enabled = true;
+            furLengthPointTxt.Enabled = true;
         }
 
         private void submitBtn_Click(object sender, EventArgs e)
         {
-            //int sizeScore = Int32.Parse(sizePointTxt.Text);
-            //int eyeScore = Int32.Parse(eyePointTxt.Text);
-            //int furColorScore = Int32.Parse(furColorPointTxt.Text);
-            //int furLengthScore = Int32.Parse(furLengthPointTxt.Text);
-            //int totalScore = (furLengthScore + furColorScore + eyeScore + sizeScore);
-            //totalPointsLbl.Text = totalScore.ToString();
-            //int eventId = Int32.Parse(eventNumTxt.Text);
-            //pull.InsertScores(totalScore, sizeScore, eyeScore, furColorScore, furLengthScore, dogID);
-            //int locationID = Int32.Parse(locationIdTxt.Text);
-            //pull.InsertEvent(locationID, eventId);
+            int sizeScore = Int32.Parse(sizePointTxt.Text);
+            int eyeScore = Int32.Parse(eyePointTxt.Text);
+            int furColorScore = Int32.Parse(furColorPointTxt.Text);
+            int furLengthScore = Int32.Parse(furLengthPointTxt.Text);
+            int totalScore = (furLengthScore + furColorScore + eyeScore + sizeScore);
+            totalPointsLbl.Text = totalScore.ToString();
+            int eventId = Int32.Parse(eventNumTxt.Text);
+            judge.InsertScores(totalScore, sizeScore, eyeScore, furColorScore, furLengthScore, dogID);
+            int locationID = Int32.Parse(locationIdTxt.Text);
+            judge.InsertEvent(locationID, dogID);
 
         }
     }
---
+++ Service_DogShow/Judgement.cs
@@ -0,0 +1,36 @@
+﻿using Common_DogShow;
+using DataAccess_DogShow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_DogShow
+{
+    public class Judgement
+    {
+        public List<DogBreed> nameList = new List<DogBreed>();
+        PullSqlData pull = new PullSqlData();
+        int dogId;
+        
+        public List<DogBreed> GetDogBreedList()
+        {
+            nameList = pull.GetDogBreed();
+            return nameList;
+        }
+        public int GetDogID(string name)
+        {
+            dogId = pull.DogIdNum(name);
+            return dogId;
+        }
+        public void InsertScores(int total, int size, int eye, int furColor, int furLength, int dogID)
+        {
+            pull.InsertScores(total, size, eye, furColor, furLength, dogID);
+        }
+        public void InsertEvent(int locationID, int EventID)
+        {
+            pull.InsertEvent(locationID, EventID);
+        }
+    }
+}
